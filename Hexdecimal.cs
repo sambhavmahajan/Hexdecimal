33bb4fc0748c39d8a137cf2315f453d78f18bd97
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2020-09-06 11:29:38 +0530

    Update Hexdecimal.cs

--- Hexdecimal.cs
+++ Hexdecimal.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,6 +43,10 @@ namespace HexDecimal
         public static int ConvertToInt(string Hex)
         {
             char[] i = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            if (!IsHexdecimal(Hex))
+            {
+                throw new System.ArgumentException("Please enter a valid hex value");
+            }
             int len = Hex.Length - 1;
             int r = 0;
             foreach (char c in Hex)
@@ -60,6 +64,18 @@ namespace HexDecimal
                 r *= x;
             }
             return r;
+        }
+        public static bool IsHexdecimal(string h)
+        {
+            char[] i = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+            foreach (char c in h)
+            {
+                if (!i.Contains(c))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
